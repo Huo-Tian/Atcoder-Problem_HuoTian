1b66b6910286fd41647f07dbb6a1a4c004fa7460
Huo-Tian <huotianyuxi@petalmail.com>
2026-07-23 09:02:48 +0800

    Knocx Update 5 (ABC453B+Note.md)

--- Atcoder-beginning contest/453/ABC453B - Sensor Data Logging.cpp
+++ Atcoder-beginning contest/453/ABC453B - Sensor Data Logging.cpp
@@ -11,17 +11,12 @@ int main() {
     for(int i = 0; i <= n; i ++) {
         cin >> a[i];
     }
-    int cnt = a[0], sym = 0;
-    b[++sym].t = 0;
-    b[sym].s = a[0]; 
+    cout << 0 << " " << a[0] << endl;
+    int rec = a[0];
     for(int i = 1; i <= n; i ++) {
-        if(abs(a[i] - cnt) >= x) {
-            cnt = a[i];
-            b[++sym].t = i;
-            b[sym].s = a[i]; 
+        if(abs(a[i] - rec) >= x) {
+            cout << i << " " << a[i] << endl;
+            rec = a[i];
         }
-    }
-    for(int i = 1; i <= sym; i ++) {
-        cout << b[i].t << " " << b[i].s << endl;
     }
 }
---
+++ Atcoder-beginning contest/Note.md
@@ -0,0 +1,26 @@
+# 本项目介绍 (2026-07-23)
+
+## 一、简介
+
+1.本项目为[Atcoder](https://atcoder.jp) 比赛题代码(有些题目前还没有代码)。
+
+全称Atcoder-Problem_HuoTian，简称APH
+
+2.现在更新到ABC413 ~ ABC467, AWC0001 ~ AWC0004
+
+3.目前为135文件(C++ + markdown, 包含.vscode、代码文件夹、Markdown)。
+
+4.每周（有可能延迟）更新~
+
+## 二、整理~
+
+现在的维护由@HuoTian2016和@Huo-Tian2016进行，现在的添加代码工作由@Huo-Tian进行。
+
+## 协议
+
+本项目协议为Apache License 2.0.
+
+Huotian© 2025-2026 允许复刻
+
+Wrote in Xi'an, Shaanxi, China.
+***Huo-Tian & HuoTian2016 & Huo-Tian2016***
