c051fe5f49b35ca467cc6356b89b6d3c3496221d
Huo-Tian <huotianyuxi@petalmail.com>
2026-07-24 22:27:46 +0800

    Knocx Update 9(432B+DZYP&APH大公告)

--- Atcoder-beginning contest/432/ABC432B - Permute to Minimize.cpp
+++ Atcoder-beginning contest/432/ABC432B - Permute to Minimize.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a[100010];
+int n, a[11];
 
 int main() {
+    cin >> n;
+    while(n) {
+        a[(n % 10)] ++;
+        n /= 10;
+    }
+
 }
