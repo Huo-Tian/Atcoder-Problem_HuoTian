e867de1256d506e9408207f218d5e79a4ed07bad
Huo-Tian <huotianyuxi@petalmail.com>
2026-07-24 19:22:47 +0800

    Knocx Update 8 (ABC441B)

--- Atcoder-beginning contest/441/ABC441B - Two Languages.cpp
+++ Atcoder-beginning contest/441/ABC441B - Two Languages.cpp
@@ -1,72 +1,41 @@
-//abc441-b
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-
-int q;
-string w;
-
-bool takahashi = false, unknown = true, aoki = false;
+int n, m, q;
+string s, t;
 bool s1[27], t1[27];
 
 int main() {
-	cin >> n >> m;
-	for(int i = 1; i <= n; i ++) {
-		char s;
-		cin >> s;
-		s1[s - 'a' + 1] = true;
-	}
-	for(int i = 1; i <= m; i++) {
-		char t;
-		cin >> t;
-		t1[t - 'a' + 1] = true;
-	}
-	cin >> q;
-	for(; q--;  ){
-		cin >> w;
-		int ws = w.size();
-		bool w1[27];
-		for(int i = 0; i < ws; i ++) {
-			w1[w[i] - 'a' + 1] = true;
-		}
-		for(int i = 1; i <= 26; i ++) {
-			if(w1[i] == true) {
-				if(s1[i] == true && t1[i] == true && !(takahashi || aoki)) {
-					unknown = true;
-					takahashi = false;
-					aoki = false;
-					break;
-				} else if(s1[i] == true && t1[i] == false){
-					if(aoki != true) {
-						unknown = false;
-						takahashi = true;	
-					} else {
-						unknown = true;
-						takahashi = false;
-						aoki = false;
-						break;
-					}
-				} else if(s1[i] == false && t1[i] == true) {
-					if(takahashi != true) {
-						unknown = false;
-						aoki = true;
-					} else {
-						unknown = true;
-						takahashi = false;
-						aoki = false;
-						break;
-					}
-				}
-			}
-		}
-		if(unknown) {
-			cout << "Unknown\n";
-		}  else if(takahashi == true) {
-			cout << "Takahashi\n";
-		} else if(aoki == true) {
-			cout << "Aoki\n";
-		}
-	}
+    cin >> n >> m;
+    cin >> s >> t;
+    for(int i = 0; i < n; i ++) {
+        s1[(int)(s[i] - 'a') + 1] = true;
+    }
+    for(int i = 0; i < m; i ++) {
+        t1[(int)(t[i] - 'a') + 1] = true;
+    }
+    cin >> q;
+    for(; q--; ) {
+        string x;
+        cin >> x;
+        int l = x.size();
+        bool unknown = false;
+        for(int i = 0; i < l; i ++) {
+            int tip = (int)(x[i] - 'a') + 1;
+            if(s1[tip] == true && t1[tip] == true) {
+                unknown = true;
+            } else if(s1[tip] == true) {
+                cout << "Takahashi\n";
+                unknown = false;
+                break;
+            } else if(t1[tip] == true) {
+                cout << "Aoki\n";
+                unknown = false;
+                break;
+            }
+        }
+        if(unknown == true) {
+            cout << "Unknown\n";
+        }
+    }
 }
